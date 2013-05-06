4cdf1bcd0c249cb85e2736ca4625d8a7c05f8117
Wouter Hordijk <wouterhordijk@gmail.com>
2013-05-06 12:13:34 +0200

    added initialization module

---
+++ CreateWorld.f90
@@ -0,0 +1,33 @@
+module CreateWorld
+
+    implicit none
+    private
+
+    public initiate_system
+
+contains
+
+    subroutine initiate_system(positions, velocities)
+
+        real(8), intent(inout) :: positions(:, :), velocities(:, :)
+        integer :: N, i, j, k, m, z
+        real(8) :: a
+
+        N = size(positions, 2)
+        m = 2
+        a = 1d0
+        z = 1
+        do i = 1, m
+            do j = 1, m
+                do k = 1, m
+                    positions(1, z) = (i - 1) * 1d0
+                    positions(2, z) = (j - 1) * 1d0
+                    positions(3, z) = (k - 1) * 1d0
+                    z = z + 1
+                end do
+            end do
+        end do
+        velocities = 0
+    end subroutine
+
+end module
--- HydroDynamics.f90
+++ HydroDynamics.f90
@@ -1,19 +1,22 @@
 program HydroDynamics
 
+    use CreateWorld
+
     implicit none
 
     integer, parameter  :: T = 10
-    integer :: N = 10
-    real(8) :: positions(3, N), velocities(3, N)
-    real(8) :: distances(3, N**2 - N)
+    integer, parameter :: N = 8
+    real(8) :: positions(3, N), velocities(3, N), distances(3, N**2)
     integer :: i
+
+    !allocate(positions(3, N), velocities(3, N), distances(3, N**2 - N))
 
     call initiate_system(positions, velocities)
     do i = 1, T
-        call find_neighbors(positions, distances)
-        call calc_accelerations(positions, distances, velocities)
-        call update_positions(positions, velocities)
-        call output_data(positions)
+        !call find_neighbors(positions, distances)
+        !call calc_accelerations(positions, distances, velocities)
+        !call update_positions(positions, velocities)
+        !call output_data(positions)
     end do
 
 end program
